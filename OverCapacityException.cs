4e02f4f8579261981570611b0f510e26299423a8
Sebastian Awatramani <sebastian.awatramani+github@gmail.com>
2018-02-20 11:19:25 -0700

    Implement OverCapacityException
    Exception thrown when attempting to add to queue beyond capacity

---
+++ OverCapacityException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_And_A.Source
+{
+    class OverCapacityException : Exception
+    {
+        public OverCapacityException()
+        {
+            Console.WriteLine("Over cap");
+        }
+    }
+}
