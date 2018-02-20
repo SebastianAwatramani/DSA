b55a53ef6ee822846b41f5c8e52dd30d3bd3a0f4
Sebastian Awatramani <sebastian.awatramani+github@gmail.com>
2018-02-19 21:48:15 -0700

    Implement binary search

--- Searcher.cs
+++ Searcher.cs
@@ -13,5 +13,34 @@ namespace DS_And_A
         {
             this.Form = Form;
         }
+        public int? BinarySearch(List<int> values, int query)
+        {
+            int max = values.Count - 1;
+            int min = 0;
+            int count = 0;
+            
+
+            while(min <= max)
+            {
+                Console.WriteLine("count: " + count);
+                count++;
+                int mid = (int)Math.Floor((min + max) / 2.0);
+                if (query == values[mid])
+                {
+                    return mid;
+                }
+                else if (query > values[mid])
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid - 1;
+                }
+            }
+            
+            return null;
+        }        
     }
 }
+
