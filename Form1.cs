68254beead9baa806a481cd566fa8f6a4d40d5d0
Sebastian Awatramani <sebastian.awatramani+github@gmail.com>
2018-02-20 11:40:19 -0700

    I/Queue testing

--- Form1.cs
+++ Form1.cs
@@ -19,15 +19,35 @@ namespace DS_And_A
             InitializeComponent();
 
             //int[] valsBalls = new int[6] { 10, 2, 8, 6, 7, 3 };
-            int[] valsBalls = new int[10] { 1, 2, 4, 8, 17, 26, 37, 48, 59,110};
-            List<int> Balls = new List<int>(valsBalls);
+            //int[] valsBalls = new int[10] { 1, 2, 4, 8, 17, 26, 37, 48, 59,110};
+            //List<int> Balls = new List<int>(valsBalls);
 
-            Searcher Searcher = new Searcher(this);
+            //Searcher Searcher = new Searcher(this);
 
-            int query = 48;
-            int? index = Searcher.BinarySearch(Balls, query);
+            //int query = 48;
+            //int? index = Searcher.BinarySearch(Balls, query);
 
-            Console.WriteLine("result: " + index);
+            //Console.WriteLine("result: " + index);
+
+            Source.Queue queue = new Source.Queue(10);
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+            queue.Enqueue(7);
+            queue.Enqueue(8);
+            queue.Enqueue(9);
+            queue.Enqueue(10);
+            queue.Enqueue(11);
+            queue.Enqueue(11);
+            for (int i = queue.Count; i > 0; i--)
+            {
+              
+                queue.Dequeue();
+            }
 
 
 
